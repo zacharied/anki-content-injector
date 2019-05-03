e77a9abe1b443656efce2085e11511dbf5ea9765
Zacharie Day <day@gwu.edu>
2019-05-03 18:58:28 -0400

    Actually commit the plugin
    > Be me
    > Have file called `globalCss.py`
    > Instead of renaming, make symlink to that file called `__init__.py`
    > Write all the code
    > Hmmm, I should clean this up before I commit
    > Delete `globalCss.py` because duh, the code is in `__init__.py`
    > Commit and push
    > Realize mistake
    > ...
    > Rewrite code

--- __init__.py
+++ __init__.py
@@ -1,1 +1,34 @@
-GlobalCss.py
+import codecs
+import os
+
+from aqt import mw
+from anki.hooks import addHook
+
+STYLESHEET_FILE = '_global.css'
+
+isEnabled = True
+
+def toggleEnabled(action):
+    global isEnabled
+    isEnabled = not isEnabled
+    action.setChecked(isEnabled)
+
+def injectGlobalCss(html, card, context):
+    global isEnabled
+    if not isEnabled:
+        return html
+
+    cssFilePath = os.path.join(mw.col.media.dir(), STYLESHEET_FILE)
+    if not os.path.isfile(cssFilePath):
+        return html
+    
+    with open(cssFilePath, 'r', encoding='utf-8') as cssFile:
+        return html + '<style>' + cssFile.read() + '</style>'
+
+mw.form.menuTools.addSeparator()
+action = mw.form.menuTools.addAction('Use Global CSS')
+action.setCheckable(True)
+action.setChecked(isEnabled)
+action.triggered.connect(lambda _, a=action: toggleEnabled(a))
+
+addHook('prepareQA', injectGlobalCss)
