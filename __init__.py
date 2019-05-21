4f7c6e91426d06a4b8ced8077cb235a6a2fd85a2
Zacharie Day <day@gwu.edu>
2019-05-21 15:28:07 -0400

    Add toggle to card layout editor
    This should allow for people to debug their global content more easily. By
    toggling the checkbox at the top of the layout editor window, the user can
    instantly tell what parts of the layout are coming from their global content.

--- __init__.py
+++ __init__.py
@@ -1,10 +1,10 @@
 import os
 
-from aqt import mw
-from anki.hooks import addHook
-
-config = mw.addonManager.getConfig(__name__)
-isEnabled = config['startEnabled']
+from aqt import mw, qt
+from aqt.clayout import CardLayout
+from anki.hooks import addHook, wrap
+from aqt.utils import showInfo
+import PyQt5
 
 EXT_TO_TAG = {
     '.html': 'div',
@@ -12,16 +12,37 @@ EXT_TO_TAG = {
     '.js': 'script'
 }
 
+config = mw.addonManager.getConfig(__name__)
+
+isEnabled = config['startEnabled']
+isEnabledPreview = isEnabled
+
 def toggleEnabled(action):
     global isEnabled
     isEnabled = not isEnabled
     action.setChecked(isEnabled)
+
+def setPreviewEnabled(check, editor):
+    global isEnabledPreview
+    isEnabledPreview = check.isChecked()
+    editor.redraw()
+
+def availableInjectFiles():
+    global config
+    files = []
+    for f in config['injectFiles']:
+        if os.path.isfile(os.path.join(mw.col.media.dir(), f)):
+            files.append(f)
+    return files
 
 def injectGlobalContent(html, card, context):
     global isEnabled
     global config
 
     if not isEnabled:
+        return html
+    
+    if not isEnabledPreview and (context == 'clayoutQuestion' or context == 'clayoutAnswer'):
         return html
 
     inject = ''
@@ -39,7 +60,20 @@ def injectGlobalContent(html, card, context):
         return html + inject
     else:
         return inject + html
-    
+
+def clayoutEditorTopArea(editor, _old):
+    global isEnabled, isEnabledPreview
+    isEnabledPreview = isEnabled
+
+    _ret = _old(editor)
+
+    checkbox = qt.QCheckBox("Inject global content in preview")
+    checkbox.setChecked(isEnabled)
+    checkbox.stateChanged.connect(lambda: setPreviewEnabled(checkbox, editor))
+    editor.topAreaForm.verticalLayout.addWidget(checkbox)
+
+    return _ret
+
 mw.form.menuTools.addSeparator()
 action = mw.form.menuTools.addAction('Inject Global Content')
 action.setCheckable(True)
@@ -47,3 +81,5 @@ action.setChecked(isEnabled)
 action.triggered.connect(lambda: toggleEnabled(action))
 
 addHook('prepareQA', injectGlobalContent)
+
+CardLayout.setupTopArea = wrap(CardLayout.setupTopArea, clayoutEditorTopArea, "around")
